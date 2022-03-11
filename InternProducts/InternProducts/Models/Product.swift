787d3dc0dce717ccb88470aba0d5aea8bd04f532
Rona Bompa <rona.bompa@endava.com>
2022-03-11 15:02:18 +0200

    initial commit

---
+++ InternProducts/InternProducts/Models/Product.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  InternProducts
+//
+//  Created by Rona Bompa on 11.03.2022.
+//
+
+import Foundation
+import UIKit
+
+struct Product {
+
+    var title: String
+    var description: String
+    var tags: String
+    var image: UIImage
+
+    // TODO: Maybe optionals?
+}
---
+++ InternProducts/InternProducts/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  InternProducts
+//
+//  Created by Rona Bompa on 11.03.2022.
+//
+
+import Foundation
+
+struct User {
+    let username: String
+    let password: String
+
+    // TODO: Init x2 & maybe default values?
+}
